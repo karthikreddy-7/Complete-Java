b83cb197117616b010529b3c14b8f6e6254e6084
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-07-30 23:49:06 +0530

    added searching algorithms

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,5 +1,42 @@
 package Searching;
 
-public class BinarySearch {
+import java.util.*;
 
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the " + (i + 1) + " value of the array");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target value:");
+        int key = sc.nextInt();
+        int value = binarysearch(arr, n, key);
+        if (value == -1) {
+            System.out.println("Target value Not Found in the array");
+        } else {
+            System.out.println("Target value found at index " + value + " in the array");
+        }
+        sc.close();
+    }
+
+    public static int binarysearch(int[] arr, int n, int key) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] > key) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+
+    }
 }
--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,5 +1,29 @@
 package Searching;
 
-public class LinearSearch {
+import java.util.*;
 
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the value of " + (i + 1) + " element in the array:");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target value:");
+        int t = sc.nextInt();
+        boolean found = false;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == t) {
+                found = true;
+                System.out.println("Target value found at " + i + " index of the array.");
+            }
+        }
+        if (!found) {
+            System.out.println("target value not found in the array.");
+        }
+        sc.close();
+    }
 }
