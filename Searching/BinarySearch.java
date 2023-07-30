76110e300822aa58767423018dfd746e102561df
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-07-30 23:54:09 +0530

    binary search

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -27,7 +27,7 @@ public class BinarySearch {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
             if (arr[mid] == key) {
                 return mid;
             } else if (arr[mid] > key) {
