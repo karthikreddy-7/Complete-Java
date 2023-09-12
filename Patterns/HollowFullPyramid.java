2a212a507a8b0356249956f9099e915d85a13c65
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-12 21:41:42 +0530

    pyramid

---
+++ Patterns/HollowFullPyramid.java
@@ -0,0 +1,32 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowFullPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of N:");
+        int n = sc.nextInt();
+        int k = 0;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = i; j < n; j++)
+                System.out.print(" ");
+            while (k != (2 * i - 1)) {
+                if (k == 0 || k == 2 * i - 2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+                k++;
+                ;
+            }
+            k = 0;
+            System.out.println();
+        }
+        // print last row
+        for (int i = 0; i < 2 * n - 1; i++)
+            System.out.print("*");
+        sc.close();
+    }
+
+}
---
+++ Patterns/InvertedFullPyramid.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class InvertedFullPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n:");
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if (j >= n - (i - 1) && j <= n + (i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
